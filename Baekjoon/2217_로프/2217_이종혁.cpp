ad3da85b50c61dafcaf801ed9d9fb2d8f5f5b6bb
JongHyeok Lee <92034921+sblee1234@users.noreply.github.com>
2023-04-14 20:56:13 +0900

    Add files via upload

---
+++ Baekjoon/2217_로프/2217_이종혁.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string s;
+	int res = 0;
+	cin >> s;
+
+	int len = s.length();
+
+
+	for (int i = 0; i < len; i++) {
+		if (s[i] != s[i - 1])
+			res++;
+	}
+
+	if (res == 0)
+		cout << 0;
+	else
+		cout << res / 2;
+}

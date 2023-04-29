0151de2b50e3ffa1925d46c36df113e0cb5779ba
JongHyeok Lee <92034921+aor9@users.noreply.github.com>
2023-04-29 21:08:24 +0900

    Add files via upload

---
+++ Baekjoon/11047_동전 0/11047_이종혁.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, k, arr[11];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int res = 0;
+
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    for (int i = n - 1; i >= 0; i--) {
+        res += k / arr[i];
+        k %= arr[i];
+    }
+
+    cout << res;
+
+    return 0;
+}

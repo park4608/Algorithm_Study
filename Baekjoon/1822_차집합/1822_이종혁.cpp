e92181acd4acda32b1b8bb97dca93ed5a70dae9e
JongHyeok Lee <92034921+aor9@users.noreply.github.com>
2023-05-05 20:50:29 +0900

    Add files via upload

---
+++ Baekjoon/1822_차집합/1822_이종혁.cpp
@@ -0,0 +1,36 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> a, b, res;
+int n, m, c;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> c;
+        a.push_back(c);
+    }
+    
+    for (int i = 0; i < m; i++) {
+        cin >> c;
+        b.push_back(c);
+    }
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    for (int i = 0; i < a.size(); i++) {
+        if (binary_search(b.begin(), b.end(), a[i])) continue;
+        res.push_back(a[i]);
+    }
+
+    cout << res.size() << "\n";
+    for (int i = 0; i < res.size(); i++)
+        cout << res[i] << " ";
+
+    return 0;
+}

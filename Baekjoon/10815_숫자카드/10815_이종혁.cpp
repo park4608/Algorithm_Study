d6e6d2549bcc71eeb8a1447bfcd5540d62b11f1c
JongHyeok Lee <92034921+aor9@users.noreply.github.com>
2023-05-05 20:48:22 +0900

    Add files via upload

---
+++ Baekjoon/10815_숫자카드/10815_이종혁.cpp
@@ -0,0 +1,26 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, a[500001], card[500001];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)  
+        cin >> a[i];
+    
+    cin >> m;
+
+    for (int i = 0; i < m; i++)
+        cin >> card[i];
+    
+    sort(a, a + n);
+
+    for (int i = 0; i < m; i++)
+        cout << binary_search(a, a + n, card[i]) << ' ';
+
+    return 0;
+}
